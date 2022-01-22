c7ad7943241a2c04d05cfdf987aa7dafa69e9b92
JacaOsom <jacekfilipski@gmail.com>
2022-01-22 16:47:58 +0100

    Cw.4 wyjątki - TRY CATCH FINALLY

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,69 @@
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        //zad 1
+        String[] tab = new String[]{"AAUDI", "BMW", "MERCEDES", "KIA", "FORD", "VOLKSWAGEN"};
+        Scanner scanner = new Scanner(System.in);
+        int index = -1;
+        boolean isIndexCorrect = false;
+
+        do {
+            try{
+                System.out.println("Podaj indeks tablicy, który chcesz wyświetlić: ");
+                index = Integer.parseInt(scanner.next());
+                if (index >= tab.length || index < 0) {
+                    System.out.println("Podajesz indeks, który wkracza poza tablicę. Rozmiar tablicy wynosi " + tab.length +
+                            " (Czyli wpisz liczbę od 0 do 5");
+                } else {
+                    System.out.println(tab[index]);
+                    isIndexCorrect = true;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Zły znak! Podaj liczbę");
+                scanner.nextLine();
+            } finally {
+                System.out.println("<BLOK FINALLY>Wykonuje się cały czas");
+            }
+        }while(!isIndexCorrect);
+
+        //zad 2
+        //a
+        try {
+            String name = null;
+            name.length();
+        } catch (NullPointerException e){
+            System.out.println("Zmienna nie jest zainicjalizowana");
+            throw e;
+        }
+
+        //b
+        try {
+            int dzielenie = 15 / 0;
+            System.out.println(dzielenie);
+        } catch(ArithmeticException e){
+            System.out.println("Nie można dzielić przez 0");
+            throw e;
+        }
+            //c
+        int[] tab2 = new int[] {1, 2, 3};
+        int i;
+        try{
+            i = Integer.parseInt(scanner.next());
+            System.out.println(tab2[i]);
+        } catch(InputMismatchException e){
+            System.out.println("Zły znak!");
+            throw e;
+        } catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Liczba poza zakresem");
+            throw e;
+        } finally {
+            System.out.println("Hmmm...");
+        }
 
         /* zad.1
         a) stworzyć tablicę 6-cio elementową, uzupełnić przykładowymi wartościami (dowolny typ tablicy),
